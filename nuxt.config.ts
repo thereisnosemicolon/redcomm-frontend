4b6497405be4159f14e31c57d668cd7619155446
thereisnosemicolon <94100178+thereisnosemicolon@users.noreply.github.com>
2023-06-09 21:56:10 +0700

    first commit

---
+++ nuxt.config.ts
@@ -0,0 +1,8 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  devtools: { enabled: true },
+  modules: ['@nuxtjs/tailwindcss'],
+  tailwindcss: {
+    cssPath: '~/public/assets/css.tailwind.css'
+  }
+})
